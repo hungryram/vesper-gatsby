1b829ffaced84a9921a48c9a017004ae8e60df62
Brad S <brad@adrenalizedigital.ca>
2022-04-14 16:40:47 -0600

    Import scripts; Set root css variables

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,39 @@
-import './static/css/styles.css'
+import React from "react"
+import 'uikit/dist/css/uikit.min.css';
+import 'uikit/dist/js/uikit.min.js';
+import './src/css/styles.css'
+import Navbar from "./src/components/globals/navbar";
+import Helmet from "react-helmet";
+import { useSiteData } from "./src/hooks";
+
+const EmbedData = () => {
+
+    const { appearance } = useSiteData()
+    const colors = appearance.data.colors
+
+    return (        
+        <Helmet>
+            <style>
+                {`:root {
+                    --primary-button-background: ${colors.button_background_color};
+                    --primary-button-text-color: ${colors.button_text_color};
+                    --primary-link-color: ${colors.links};
+                    --primary-accent-color: ${colors.accent};
+                    --idx-title-color: ${colors.idx_title_background_color};
+                    --idx-title-text-color: ${colors.idx_title_text_color};
+                `}
+            </style>
+        </Helmet>
+    )
+}
+
+export const wrapPageElement = ({ element }) => {
+
+    return(
+        <>
+            <EmbedData/>
+            <Navbar/>
+            {element}
+        </>
+    )
+}
