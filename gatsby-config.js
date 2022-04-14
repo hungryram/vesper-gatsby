d0a8c7ccdd6778946a6a40195535c5abccc661e8
Brad S <brad@adrenalizedigital.ca>
2022-04-14 16:39:31 -0600

    Add transformer plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,31 @@
 module.exports = {
   plugins: [
+    `gatsby-transformer-yaml`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/data`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `main`,
+        path: `${__dirname}/content/main`
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `uploads`,
         path: `${__dirname}/static/uploads`,
-      },
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
@@ -14,9 +34,47 @@ module.exports = {
         path: `${__dirname}/content/blog`
       }
     },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    `gatsby-transformer-remark`,
-    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `legal`,
+        path: `${__dirname}/content/legal`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `listings`,
+        path: `${__dirname}/content/listings`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `offices`,
+        path: `${__dirname}/content/offices`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `our-team`,
+        path: `${__dirname}/content/our-team`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `partners`,
+        path: `${__dirname}/content/partners`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `press`,
+        path: `${__dirname}/content/press`
+      }
+    },
   ],
 }
