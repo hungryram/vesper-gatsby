60ccd25a16b05ec8cc7d7ebc49a6369df5757f0e
Brad S <brad@adrenalizedigital.ca>
2022-04-19 11:03:17 -0600

    Build contact component

--- src/components/templates/contact.js
+++ src/components/templates/contact.js
@@ -1,10 +1,31 @@
 import * as React from 'react'
+import { useSiteData } from '../../hooks'
 
 const Contact = () => {
+
+    const { profile } = useSiteData()
+    const footerContact = profile.data.contact_information
 
     return(
 
         <>
+            {footerContact.phone ?
+                <li><span className="uk-margin-small-right" data-uk-icon="receiver"></span><a href={`tel:${footerContact.phone}`}>{footerContact.phone}</a></li>
+            : null}
+            {footerContact.cell ?
+                <li><span className="uk-margin-small-right" data-uk-icon="phone"></span><a href={`tel:${footerContact.cell}`}>{footerContact.cell}</a></li>
+            : null}
+            {footerContact.email ?
+                <li><span className="uk-margin-small-right" data-uk-icon="mail"></span><a href={`mailto:${footerContact.email }`}>{footerContact.email }</a></li>
+            : null}
+            {footerContact.address ?
+                <li>
+                    <span className="uk-margin-small-right" data-uk-icon="location"></span>
+                    <a target="_blank" href={footerContact.google_page_link ? footerContact.google_page_link : `https://www.google.com/maps/place/${footerContact.address},+${footerContact.city},+${footerContact.state}`}>
+                        {footerContact.address}, {footerContact.city}, {footerContact.state} {footerContact.zip_code}
+                    </a>
+                </li>
+            : null}
         </>
 
     )
