3a2f9d7afcaf769e4faf019ed8c1946c1483e51a
Brad S <brad@adrenalizedigital.ca>
2022-04-19 10:48:04 -0600

    Build social component

--- src/components/templates/social.js
+++ src/components/templates/social.js
@@ -1,11 +1,39 @@
 import * as React from 'react'
+import { useSiteData } from '../../hooks'
 
 const Social = () => {
+
+    const { profile } = useSiteData()
+    const companySocial = profile.data.social_media 
     
     return(
 
-        <>
-        </>
+        <div class="social_icons">
+            {companySocial.facebook ?
+                <a href={companySocial.facebook} className="uk-icon-button uk-margin-small-right" data-uk-icon="icon: facebook; ratio: .8" target="_blank" rel="noreferrer"/>
+            : null}
+            {companySocial.instagram ?
+                <a href={companySocial.instagram} className="uk-icon-button uk-margin-small-right" data-uk-icon="icon: instagram; ratio: .8" target="_blank" rel="noreferrer"/>
+            : null}
+            {companySocial.linkedin ?
+                <a href={companySocial.linkedin} className="uk-icon-button uk-margin-small-right" data-uk-icon="icon: linkedin; ratio: .8" target="_blank" rel="noreferrer"/>
+            : null}
+            {companySocial.pinterest ?
+                <a href={companySocial.pinterest} className="uk-icon-button uk-margin-small-right" data-uk-icon="icon: pinterest; ratio: .8" target="_blank" rel="noreferrer"/>
+            : null}
+            {companySocial.twitter ?
+                <a href={companySocial.twitter} className="uk-icon-button uk-margin-small-right" data-uk-icon="icon: twitter; ratio: .8" target="_blank" rel="noreferrer"/>
+            : null}
+            {companySocial.youtube ?
+                <a href={companySocial.youtube} className="uk-icon-button uk-margin-small-right" data-uk-icon="icon: youtube; ratio: .8" target="_blank" rel="noreferrer"/>
+            : null}
+            {companySocial.tiktok ?
+                <a href={companySocial.tiktok} className="uk-icon-button uk-margin-small-right" data-uk-icon="icon: tiktok; ratio: .8" target="_blank" rel="noreferrer"/>
+            : null}
+            {companySocial.yelp ?
+                <a href={companySocial.yelp} className="uk-icon-button uk-margin-small-right" data-uk-icon="icon: yelp; ratio: .8" target="_blank" rel="noreferrer"/>
+            : null}
+        </div>
 
     )
 }
