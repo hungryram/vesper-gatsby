665cd072c6d75bfe409a05f170dfbc57fd2435df
Brad S <brad@adrenalizedigital.ca>
2022-04-19 10:51:52 -0600

    Create UIKit Wrapper to render UIKit icons in DOM

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,28 @@
-import React from "react"
-import 'uikit/dist/js/uikit.min.js'
-import 'uikit/dist/js/uikit-icons.js'
+import * as React from "react"
+import 'uikit/dist/js/uikit.min'
+import uikitMin from "uikit/dist/js/uikit.min.js"
+import uikitIcons from "uikit/dist/js/uikit-icons.js"
 import './src/css/styles.css'
 import Navbar from "./src/components/globals/navbar";
 import Helmet from "react-helmet";
 import { useSiteData } from "./src/hooks";
+import Footer from "./src/components/globals/footer";
+
+const UIKitWrapper = ({ children }) => {
+    React.useEffect(() => {
+        uikitMin.use(uikitIcons)
+    })
+    return(
+        <>{children}</>
+    )
+}
+
 
 const EmbedData = () => {
 
-    const { appearance, developer } = useSiteData()
+    const { appearance, developer, profile } = useSiteData()
     const colors = appearance.data.colors
+    const seo = profile.data.search_engine_optimization
 
     return (        
         <Helmet>
@@ -23,7 +36,17 @@ const EmbedData = () => {
                     --idx-title-text-color: ${colors.idx_title_text_color};
                 `}
             </style>
+
+            {/* --- START SEO --- */}
+            <meta name="twitter:site" content={seo.twitter_card.twitter_username} />
+            <meta name="twitter:creator" content={seo.twitter_card.twitter_username} />
+
+            {/* --- CUSTOM CSS --- */}
             {developer.data.custom_css}
+
+            {/* --- FONTS --- */}
+            <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;1,100;1,300;1,400;1,700&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"/>
+            
         </Helmet>
     )
 }
@@ -31,10 +54,11 @@ const EmbedData = () => {
 export const wrapPageElement = ({ element }) => {
 
     return(
-        <>
+        <UIKitWrapper>
             <EmbedData/>
             <Navbar/>
             {element}
-        </>
+            <Footer/>
+        </UIKitWrapper>
     )
 }
