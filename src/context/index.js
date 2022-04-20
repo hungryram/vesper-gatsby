d205d1eff6f9b1acb821fe2702a7f899e88096b5
Brad S <brad@adrenalizedigital.ca>
2022-04-20 09:51:36 -0600

    Create site context provider

---
+++ src/context/index.js
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import { useSiteData } from '../hooks'
+
+const Site = React.createContext()
+
+const SiteProvider = ({ children }) => {
+
+    const siteData = useSiteData()
+
+    const appearance = siteData.appearance.data
+    const profile = siteData.profile.data
+    const developer = siteData.developer.data 
+    const footer = siteData.footer.data 
+    const menu = siteData.menu.data
+
+    return(
+        <Site.Provider value={{ appearance, profile, developer, footer, menu }}>
+            {children}
+        </Site.Provider>
+
+    )
+}
+
+export { Site, SiteProvider }
